bdc1eaeced1bb9c2adfffcf5895637663e15d0ea
zanzlender <a1zan.zlender@gmail.com>
2022-12-10 22:33:26 +0100

    Updated examples

--- public/example_static/web/crud.js
+++ public/example_static/web/crud.js
@@ -1,6 +1,6 @@
 (() => {
   const sendBtn = document.getElementById("getUsersBtn");
-  const btnContainer = document.getElementById("jsContainerTest");
+  const usersContainer = document.getElementById("jsContainerTest");
 
   sendBtn.addEventListener("click", async () => {
     const url = document.getElementById("requestUrlInput").value;
@@ -11,13 +11,32 @@
     });
 
     try {
-      const newEl = document.createElement("p");
-      newEl.textContent = response;
+      const body = await response.json();
+
+      usersContainer.childNodes.forEach((x) => {
+        usersContainer.removeChild(x);
+      });
+
+      if (Array.isArray(body)) {
+        body.forEach((x) => {
+          const newEl = document.createElement("p");
+          const textToDisplay = `Name: ${x?.name}\nEmail: ${x?.email}`;
+
+          newEl.textContent = textToDisplay;
+          usersContainer.appendChild(newEl);
+        });
+      } else {
+        const newEl = document.createElement("p");
+        const textToDisplay = `Name: ${body?.name}\nEmail: ${body?.email}`;
+
+        newEl.textContent = textToDisplay;
+        usersContainer.appendChild(newEl);
+      }
     } catch (error) {
       const newEl = document.createElement("p");
       newEl.textContent = "Error while fetching data.";
 
-      btnContainer.appendChild(newEl);
+      usersContainer.appendChild(newEl);
     }
   });
 })();
